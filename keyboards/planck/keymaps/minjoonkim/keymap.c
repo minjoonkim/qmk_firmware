edc5f2c5d4b1b41c25ddee773ee74444875e2e51
Minjoon Kim <minjoon.kim89@gmail.com>
2022-01-20 16:07:59 +0900

    screen shot fix

--- keyboards/planck/keymaps/minjoonkim/keymap.c
+++ keyboards/planck/keymaps/minjoonkim/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  ANY(G(S(KC_4))),  ANY(G(S(KC_3))),  _______, _______, KC_HOME, KC_END,  _______,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  LGUI(LSFT(KC_4)),  LGUI(LSFT(KC_3)),  _______, _______, KC_HOME, KC_END,  _______,
     _______, _______, _______, _______, _______, _______, _______, _______,    KC_MPRV,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
